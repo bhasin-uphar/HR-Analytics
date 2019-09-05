6f20b414ecd8fe764df3276c1736be5286b072eb
bhasin-uphar <37235597+bhasin-uphar@users.noreply.github.com>
2019-09-05 10:41:47 +0530

    Using XGBoost Algorithm

--- hr.py
+++ hr.py
@@ -1,6 +1,4 @@
 import xgboost as xgb
-from sklearn.ensemble import RandomForestRegressor
-
 from sklearn.metrics import mean_squared_error
 import pandas as pd
 import numpy as np
@@ -28,25 +26,17 @@ excel['employee_id'] = test_data.employee_id
 data = data.drop(['region','employee_id'], axis=1)
 test_data = test_data.drop(['region','employee_id'], axis=1)
 
-data = data.fillna(value=0)
-test_data = test_data.fillna(value=0)
-
 X, y = data.iloc[:,:-1],data.iloc[:,-1]
 data_dmatrix = xgb.DMatrix(data=X,label=y)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=123)
 
-clf2 = xgb.XGBRegressor(objective ='reg:linear', colsample_bytree = 0.4, learning_rate = 0.8,
+xg_reg = xgb.XGBRegressor(objective ='reg:linear', colsample_bytree = 0.4, learning_rate = 0.8,
                 max_depth = 8, alpha = 20, n_estimators = 20)
 
-clf1 = RandomForestRegressor(n_estimators='warn', criterion='mse', max_depth=None, min_samples_split=2, min_samples_leaf=1,
-                             min_weight_fraction_leaf=0.0, max_features='auto', max_leaf_nodes=None, min_impurity_decrease=0.0,
-                             min_impurity_split=None, bootstrap=True, oob_score=False, n_jobs=None, random_state=None, verbose=0, warm_start=False)
+xg_reg.fit(X_train,y_train)
 
-clf=clf1
-clf.fit(X_train,y_train)
-
-preds = clf.predict(test_data)
+preds = xg_reg.predict(test_data)
 
 excel['is_promoted'] = preds.astype('int')
 excel.to_csv("Hr(clean)1.csv", index=False)
